d8b37c5e6b9dfef381230aaf5ad51565cb126212
SajiBeats <SajjadAhmadi.Yektanet@gmail.com>
2025-04-21 18:38:39 +0330

    Define api calls using axios

---
+++ src/api/endpoints/market.ts
@@ -0,0 +1,25 @@
+import type { AxiosRequestConfig } from 'axios';
+import { axiosInstance } from '../axios';
+
+type ID = string | number;
+
+interface IGetOrdersParams {
+  marketId: ID;
+  type: 'buy' | 'sell';
+}
+
+interface IGetTransactionsParams {
+  marketId: ID;
+}
+
+export const getMarkets = (config?: AxiosRequestConfig) => {
+  return axiosInstance.get('/v1/mkt/markets/', config);
+};
+
+export const getMarketOrders = ({ marketId, type }: IGetOrdersParams, config?: AxiosRequestConfig) => {
+  return axiosInstance.get(`/v2/mth/actives/${marketId}/?type=${type}`, config);
+};
+
+export const getMarketTransactions = ({ marketId }: IGetTransactionsParams, config?: AxiosRequestConfig) => {
+  return axiosInstance.get(`/v1/mth/matches/${marketId}/`, config);
+};
